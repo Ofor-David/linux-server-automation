b549bcd72d355b1120415c4dc3d9e6f9bd0c0fd0
Ofor David <davidoforincloud@gmail.com>
2025-03-18 21:01:14 +0100

    feat: print disk usage

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+ 
+ set -e # exits if any command fails
 # collect cpu
 cpu_usage=$(echo "100 - $(mpstat 1 1 | awk '/Average/ {print $NF}')" | bc)
 echo "CPU usage: $cpu_usage%"
-if ($cpu_usage > 60); then
 
 # collect ram
 memory_usage=$(free | awk '/Mem/ {print $3/$2 * 100}')
-echo "Memory usageL: $memory_usage%"
+echo "Memory usage: $memory_usage%"
+
 # collect disk
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+echo "Disk usage: $disk_usage"
